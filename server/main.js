31cb5f0def677fb12140a621a0a5e2f9be69afcc
k-gitest <adobe_blow@yahoo.co.jp>
2022-04-17 18:29:19 +0000

    apiサーバー設定

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// 送信されたdataを処理するためには、body-parserモジュールを使用する
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8000
 const mysql = require('mysql')
@@ -13,13 +15,6 @@ const con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log('Connected');
-  
-  const sql = "select * from users"
-  	con.query(sql, function (err, result, fields) {  
-  	if (err) throw err;  
-  	console.log(result)
-  	console.log(result[0].email)
-	});
 	
   /* DBを作成する場合
   con.query('CREATE DATABASE mydb', function (err, result) {
@@ -39,10 +34,89 @@ con.connect(function(err) {
 	con.query(sql, function (err, result, fields) {  
 	if (err) throw err;  
 	console.log(result)
+	console.log(result[0].email)
 	});
+  */
+  /* データ挿入
+  const sql = "INSERT INTO users(name,email) VALUES('kevin','kevin@test.com')"
+  con.query(sql,function(err, result, fields){
+  	if (err) throw err;
+  	console.log(result)
+  })
+  
+  // データ挿入2
+  const sql = "INSERT INTO users(name,email) VALUES(?,?)"
+  con.query(sql,['Jack','jack@exsample.co.jp'],function(err, result, fields){
+  	if (err) throw err;
+  	console.log(result)
+  })
+  
+  // データ挿入3
+  const sql = "INSERT INTO users SET ?"
+  con.query(sql,{name:'Tommy',email:'tommy@test.co.jp'},function(err, result, fields){
+  	if (err) throw err;
+  	console.log(result)
+  });
   */
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static('server'));
+// body-parserをミドルウェアとして設定
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/form.html')
+})
+
+// apiサーバ設定
+app.get("/api", function(req, res, next) {
+  
+  const sql = "select * from users"
+	con.query(sql, function (err, result, fields) {  
+	if (err) throw err;
+	// CORS対応
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  );
+
+  // データの返却
+  res.json(result);
+	});
+});
+
+// 送信データをDBへ挿入
+app.post('/', (req, res) => {
+  //res.send(req.body)
+  const sql = "INSERT INTO users SET ?"
+	con.query(sql,req.body,function(err, result, fields){
+		if (err) throw err;
+		console.log(result);
+		//res.send('登録が完了しました');
+    res.redirect('/') //送信後のページ遷移
+	});
+})
+
+/*
+app.get('/', (request, response) => {
+	const sql = "select * from users"
+	con.query(sql, function (err, result, fields) {  
+	if (err) throw err;
+	//レスポンスデータを表示
+	//response.send('Hello World!')
+	response.send(result)
+	});
+});
+*/
+// コンソールに接続を表示
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
+
