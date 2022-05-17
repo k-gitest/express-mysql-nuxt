aa60e7b6a00a818a3a4380bae1205e74f8a7d4e0
k-gitest <adobe_blow@yahoo.co.jp>
2022-05-17 18:02:41 +0000

    formコンポーネント設計

---
+++ components/FormParts/DatePicker.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div v-for="(option, index) in options" :key="index">
+      <input 
+        type="radio" 
+        :name="name"
+        :value="option.value" 
+        :checked="option.value === value"
+        @change="onChange($event.target.value)"
+      />
+      {{ option.label }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    inheritAttrs: false,
+    props: {
+      options:{
+       type: Array,
+       //require: true,
+      },
+      value:{
+        type: String,
+      },
+      name:{
+        type: String,
+      }
+    },
+    
+    mounted() {
+      console.log(this.$attrs)
+    },
+    
+    methods: {
+      onChange: function(value){
+        //console.log(value)
+        this.$emit('input', value)
+      }
+    }
+    
+  }
+</script>
+
+<style scope>
+  .form-input{
+    background: #fff;
+    border: solid 1px #ccc;
+    width:100%;
+    margin:0 0 1rem 0;
+    padding: 0.2rem;
+  }
+</style>
